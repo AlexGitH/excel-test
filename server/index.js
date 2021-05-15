208f402314cb8884740c60d04c72a49279967028
AlexP <alexander.pristavka@gmail.com>
2021-05-16 01:55:03 +0300

    Create graphql server with console;

--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
 const express = require( 'express' )
-const bodyParser = require( 'body-parser' )
-
-const port = 3001
-
 const app = express()
-// app.use( express.static( PUBLIC ) );
+const mongoose = require( 'mongoose' )
+const { graphqlHTTP } = require( 'express-graphql' );
 
-app.use( bodyParser.json() )
+// const port = 3001 // common express port;
+const port = 3000 // use graphiql on react port for testing queries
 
-app.get( '/test', async( req, res ) => {
-  res.type( 'json' );
-  res.send( { status: 'ok', response: 'SERVER WORKS!' } );
-} )
+const schema = require( './schema' );
 
-app.listen( port, () => {
-  console.log( `Example app listening at http://localhost:${port}` )
-} )
+mongoose.connect( 'mongodb://mongo-1:27017/a-excel', {
+  useUnifiedTopology : true,
+  useFindAndModify   : false,
+  useNewUrlParser    : true
+} );
+const dbConnection = mongoose.connection;
+
+dbConnection.on( 'error', err => console.log( `Connection error: ${err}` ) )
+dbConnection.on( 'open', () => console.log( 'Connected to DB!' ) )
+
+  app.use( '/graphql', graphqlHTTP( async() => ( {
+    schema,
+    graphiql : true, //console
+    pretty   : true
+  } ) ) )
+  .listen( port, function( err_ ) {
+    console.log( `GraphQL Server is now running on localhost:${port}` );
+  } );
