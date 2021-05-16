1882e16e957e6db909a0643e6e3b7aa0ee674fcd
AlexP <alexander.pristavka@gmail.com>
2021-05-16 23:55:22 +0300

    Include UserInfo placeholder into App;

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 // import logo from './logo.svg';
+import UserInfo from './components/UserInfo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <UserInfo />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
