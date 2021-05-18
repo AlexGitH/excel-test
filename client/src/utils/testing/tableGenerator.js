b448174c570cf23b897db81bda1623766f3c7372
AlexP <alexander.pristavka@gmail.com>
2021-05-18 16:43:30 +0300

    Create custom tableGenerator module;

---
+++ client/src/utils/testing/tableGenerator.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-return-assign */
+
+/**
+ * Create a column template
+ *
+ * @returns {Object} Object
+ *
+ * @param {String|Number} dataIndex key
+ * @param {Function} renderFn cell rendering callback
+ */
+const buildColumn = ( dataIndex, renderFn ) => ( {
+  dataIndex,
+  title  : dataIndex,
+  key    : `col-${dataIndex}`,
+  render : ( text, record, index ) => renderFn( text, record, index )
+} )
+
+/**
+ * creates testing sourseData
+ * @return {Array[Object]} array of rows with columns
+ *
+ * @param {number} colNum number of columns
+ * @param {number} rowNum number of rows
+ */
+const buildData = ( colNum, rowNum ) =>
+  Array.from( { length: rowNum }, ( _, r ) =>
+    Array( colNum ).fill( 0 )
+        // eslint-disable-next-line no-sequences
+        .reduce( ( ac, _, x ) => ( ac[x] = `${x}-${r}`, ac ) //testing only
+        , {id: r} ) )
+
+module.exports = {
+  buildColumn,
+  buildData
+}
