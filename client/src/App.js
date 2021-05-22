9409b3b6958017a157987dce23d89e11a53e6bd9
AlexP <alexander.pristavka@gmail.com>
2021-05-23 00:32:10 +0300

    Include registration form into App using redux connect;

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import {Provider, connect}   from 'react-redux';
 import store from './reducers/store';
-import {actionGetUsers} from './reducers/actions';
+import {actionGetUsers, actionFullLogin, actionFullRegister } from './reducers/actions';
 // import {
 //   actionFullLogin,
 //   actionFullRegister,
@@ -9,6 +9,7 @@ import {actionGetUsers} from './reducers/actions';
 import DocumentEditor from './components/DocumentEditor'
 import NormalLoginForm from './components/LoginForm';
 import { Row, Col } from 'antd';
+import RegistrationForm from './components/RegistrationForm';
 // import Layout from 'antd/lib/layout/layout';
 
 // NOTE: for testing only
@@ -19,6 +20,21 @@ const CDocumentEditor = connect(state=>({
   onGetUsers : ()=>dispatch( actionGetUsers() )
 }))(DocumentEditor);
 
+const CNormalLoginForm= connect(state=>({
+  auth: state.promise.auth?.payload,
+  users: state.promise.users?.payload,
+}), dispatch=>({
+  onLogin : ( l, p )=>dispatch( actionFullLogin( l, p) )
+}))(NormalLoginForm);
+
+
+const CRegistrationForm= connect(state=>({
+  auth: state.promise.auth?.payload,
+  users: state.promise.users?.payload,
+}), dispatch=>({
+  onRegister : (  firstName,lastName, email, login, password )=>dispatch( actionFullRegister( firstName,lastName, email, login, password ) )
+}))(RegistrationForm);
+
 function App() {
   return (
     <Provider store={store}>
@@ -27,7 +43,8 @@ function App() {
         <Row justify="center" align="middle" style={{ minHeight: '100vh' }}>
           <Col span={8} >
 
-            <NormalLoginForm />
+            {/* <CNormalLoginForm /> */}
+            <CRegistrationForm />
 
           </Col>
         </Row>
