f41d223425993241199b15c5a596a160c46476c0
AlexP <alexander.pristavka@gmail.com>
2021-05-28 12:45:46 +0300

    Switch app to spreadsheet page;

--- client/src/App.js
+++ client/src/App.js
@@ -38,20 +38,21 @@ function App() {
     <Provider store={store}>
       <div className="App">
         {/* <UserInfo /> */}
-        {/* <Row justify="center" align="middle" style={{ minHeight: '100vh' }}> */}
-        <Row justify="center" style={{ minHeight: '100vh' }}>
-          {/* <Col span={8} > */}
+
+
+        {/* <Row justify="center" style={{ minHeight: '100vh' }}>
           <Col xs={24} sm={22} md={18} lg={16} xl={12} >
-            {/* <h1 align="center" > Login </h1>
-            <CNormalLoginForm /> */}
+            // TODO: add Routes
+            <h1 align="center" > Login </h1>
+            <CNormalLoginForm />
 
             <h1 align="center" > Registration </h1>
             <CRegistrationForm />
 
           </Col>
-        </Row>
+        </Row> */}
 
-        {/* <CDocumentEditor /> */}
+        <CDocumentEditor />
       </div>
     </Provider>
   );
