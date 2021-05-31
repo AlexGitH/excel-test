1b8434181184b27fcd27eeb7f320ff4fbfbf4da5
AlexP <alexander.pristavka@gmail.com>
2021-05-31 17:41:10 +0300

    Create letter to number converting function;

--- client/src/utils/testing/letterToNumberConverter.js
+++ client/src/utils/testing/letterToNumberConverter.js
@@ -1,38 +1,33 @@
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const base = letters.length;
-// const range = [...letters].reduce( ( res, ltr, idx ) => {
-//   res[ltr] = idx + 1;  // TODO:  think about 0 as starting index
-//   return res;
-// }, {} );
+const letterToNumberMap = [...letters].reduce( ( res, ltr, idx ) => {
+  res[ltr] = idx + 1;  // TODO:  think about 0 as starting index
+  return res;
+}, {} );
 
-// const re = /^[A-Z]+$/;
+const re = /^[A-Z]+$/;
 
-// function lettersToNumber( str ) {
-//   if ( !re.test( str ) ) throw new Error( 'Column position must consists of A-Z characters' );
-
-//   [...str].reduce( ( ac, letter ) => {
-//     return 'hello';
-//   } );
-
-//   return false;
-// }
+function lettersToNumber( str ) {
+  if ( !re.test( str ) ) throw new Error( 'Column position must consists of A-Z characters' );
+  const [ leftLtr, rightLtr ] = [...str.slice( str.length - 2 )];
+  const [ left, right ] = [ letterToNumberMap[leftLtr], letterToNumberMap[rightLtr] ];
+  return left * base + right;
+}
 
 function letterFromNumber( num ) {
   return num > 0 ? letters[num - 1] : letters[base - 1];
 }
 
-// TESTS
-// Array.from({length: 300}).map((_,i)=>CA.numberToLetters(i+1)).join()
-
-function numberToLetters( num ) {
+function numberToLetters( input ) {
+  const num = parseInt( input );
+  if ( isNaN( num ) && num <= 0 ) throw new Error( 'Argument must be a positive number' );
   const temp = Math.trunc( num / base );
   const right = num % base;
-  const left = right === 0 ? temp - 1 : temp;
+  const left = right <= 0 ? temp - 1 : temp;
 
   const result = [];
 
   result.push( letterFromNumber( right ) );
-  // result.unshift( left > base ? numberToLetters( left ) : letterFromNumber( left ) );
   // TODO: find solution for columns greater ZZ
   if ( left >= 1 && left <= base ) {
     result.unshift( letterFromNumber( left ) );
@@ -41,6 +36,11 @@ function numberToLetters( num ) {
 }
 
 module.exports = {
-  // lettersToNumber,
+  lettersToNumber,
   numberToLetters
 };
+
+// CRITICAL TESTS
+// CA.lettersToNumber('ZZ') // 702
+// `${CA.numberToLetters(26)} ${CA.numberToLetters(27)}`
+// Array.from({length: 300}).map((_,i)=>CA.numberToLetters(i+1)).join()
